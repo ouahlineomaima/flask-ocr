60161586edae24b6ef464ee39de236cdf1cbd52d
ouahline.omaima <ouahline.omaima@ine.inpt.ma>
2023-02-28 18:32:58 +0100

    bad request problem

--- api/index.py
+++ api/index.py
@@ -17,23 +17,29 @@ def about():
     return 'About'
 
 
-@app.route('/', methods=['POST'])
+@app.route('/extract_words', methods=["GET", 'POST'])
 def extract_words():
-    # Get the image URI from the request
-    image_uri = request.json['image_uri']
+    if request.method == 'GET':
+        return "hi"
+    if request.method == 'POST':
+        # Get the image URI from the request
+        image_uri = request.json['image_uri']
 
-    # Download the image from the URI
-    #response = requests.get(image_uri)
+        # Download the image from the URI
+        #response = requests.get(image_uri)
 
-    # Load the image into a PIL Image object
-    img = Image.open(image_uri)
+        # Load the image into a PIL Image object
+        img = Image.open(image_uri)
 
-    # Use Pytesseract to extract text from the image
-    text = pytesseract.image_to_string(img)
+        # Use Pytesseract to extract text from the image
+        text = pytesseract.image_to_string(img)
 
-    # Split the text into words
-    words = text.split()
+        # Split the text into words
+        words = text.split()
 
-    # Return the list of words as a JSON response
-    return jsonify({'words': words})
+        # Return the list of words as a JSON response
+        return jsonify({'words': words})
+
+if __name__ == '__main__':
+    app.run()
 
