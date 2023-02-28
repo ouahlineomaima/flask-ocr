e757b48511d8a6653c3c59a31bc3e3efb1662f3e
ouahline.omaima <ouahline.omaima@ine.inpt.ma>
2023-02-28 17:23:38 +0100

    adding the OCR API

--- api/index.py
+++ api/index.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import pytesseract
+from PIL import Image
 
 app = Flask(__name__)
 
@@ -9,3 +11,23 @@ def home():
 @app.route('/about')
 def about():
     return 'About'
+@app.route('/', methods=['POST'])
+def extract_words():
+    # Get the image URI from the request
+    image_uri = request.json['image_uri']
+
+    # Download the image from the URI
+    #response = requests.get(image_uri)
+
+    # Load the image into a PIL Image object
+    img = Image.open(image_uri)
+
+    # Use Pytesseract to extract text from the image
+    text = pytesseract.image_to_string(img)
+
+    # Split the text into words
+    words = text.split()
+
+    # Return the list of words as a JSON response
+    return jsonify({'words': words})
+
