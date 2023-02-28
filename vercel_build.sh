3ba0d995f1b7ecd1ef39597b9fd73f9119901183
ouahline.omaima <ouahline.omaima@ine.inpt.ma>
2023-02-28 21:09:27 +0100

    adding the Tesseract engine2

--- vercel_build.sh
+++ vercel_build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Update package lists
-sudo apt-get update
+apt-get update
 
 # Install Tesseract and other dependencies
-sudo apt-get install -y tesseract-ocr libtesseract-dev libicu-dev libicu-dev libicu-dev
+apt-get install -y tesseract-ocr libtesseract-dev libicu-dev libicu-dev libicu-dev
 
 # Install Python packages
 pip install -r requirements.txt
