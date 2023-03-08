2aca693edf6b39c35f340085bda491c3f14d1940
ouahline.omaima <ouahline.omaima@ine.inpt.ma>
2023-03-08 09:43:52 +0100

    deploying on render

--- vercel_build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Install Tesseract and other dependencies
-sudo apt install -y tesseract-ocr libtesseract-dev libicu-dev libicu-dev libicu-dev
-
-# Install Python packages
-pip install -r requirements.txt
